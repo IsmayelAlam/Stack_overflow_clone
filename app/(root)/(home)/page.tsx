34b85bd49bcb7db50770d38c0699996dc9283f98
Ismayel Alam <ismayelalam0@gmail.com>
2023-12-16 16:06:47 +0600

    add theme provider for dark mode

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,8 @@
 import { UserButton } from "@clerk/nextjs";
-import React from "react";
 
 export default function Home() {
   return (
     <main>
-      <h1>home</h1>
       <UserButton afterSignOutUrl="/" />
     </main>
   );
---
+++ context/ThemeProvider.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+interface ThemeContextType {
+  mode: string;
+  setMode: (mode: string) => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export default function ThemeProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [mode, setMode] = useState("light");
+
+  const handleThemeChange = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.documentElement.classList.add("dark");
+      document.documentElement.classList.remove("light");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.add("light");
+    }
+  };
+
+  // useEffect(() => handleThemeChange(), [mode]);
+
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+
+  if (context === undefined) {
+    throw new Error("theme context used outside of theme provider");
+  }
+
+  return context;
+}
