59e4e1d5427c7bea32b26d380142257fd83d5a01
Ismayel Alam <ismayelalam0@gmail.com>
2023-12-18 19:24:10 +0600

    add no result component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,11 @@
 import HomeFilter from "@/components/home/HomeFilter";
 import Filter from "@/components/shared/Filter";
+import NoResults from "@/components/shared/NoResults";
 import LocalSearch from "@/components/shared/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
+import { title } from "process";
 
 export default function Home() {
   return (
@@ -31,6 +33,48 @@ export default function Home() {
         />
       </div>
       <HomeFilter />
+      <div className="mt-10 flex flex-col gap-6">
+        {ques.length > 0 ? (
+          ques.map((q) => "q")
+        ) : (
+          <NoResults
+            title="There is no quesiton to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 }
+
+const ques = [];
+// [
+//   {
+//     _id: 1,
+//     title: "python import function?",
+//     tags: [
+//       { _id: 1, name: "python" },
+//       { _id: 2, name: "sql" },
+//     ],
+//     author: "jhon",
+//     upvotes: 10,
+//     views: 100,
+//     answers: 2,
+//     createdAt: "2023-09-01T12:00:00.000Z",
+//   },
+//   {
+//     _id: 2,
+//     title: "Center a div in html?",
+//     tags: [
+//       { _id: 1, name: "html" },
+//       { _id: 2, name: "css" },
+//     ],
+//     author: "Jassy",
+//     upvotes: 8,
+//     views: 120,
+//     answers: 5,
+//     createdAt: "2023-09-01T12:00:00.000Z",
+//   },
+// ];
