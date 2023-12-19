76d720e8e0feaa38fbee914ba15edd49a23caf4b
Ismayel Alam <ismayelalam0@gmail.com>
2023-12-19 11:21:34 +0600

    add tagging function for the ask question page

--- components/forms/Questions.tsx
+++ components/forms/Questions.tsx
@@ -14,25 +14,59 @@ import { Input } from "@/components/ui/input";
 import { questionSchema } from "@/lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Editor } from "@tinymce/tinymce-react";
-import { useRef } from "react";
+import React, { useRef } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { Badge } from "../ui/badge";
+import Image from "next/image";
+import { useTheme } from "@/hooks/useTheme";
 
 export default function Questions() {
   const editorRef = useRef(null);
+  const { mode } = useTheme();
 
   const form = useForm<z.z.infer<typeof questionSchema>>({
     resolver: zodResolver(questionSchema),
     defaultValues: {
       title: "",
-      explanation: "",
-      tag: [],
+      tags: [],
     },
   });
 
   function onSubmit(values: z.infer<typeof questionSchema>) {
     console.log(values);
   }
+
+  const handleInputKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: any
+  ) => {
+    if (e.key === "Enter" && field.name === "tags") {
+      e.preventDefault();
+
+      const tagInput = e.target as HTMLInputElement;
+      const tagValue = tagInput.value.trim();
+      if (tagValue !== "") {
+        if (tagValue.length > 15) {
+          return form.setError("tags", {
+            type: "required",
+            message: "Tag must be less than 15 characters",
+          });
+        }
+        if (!field.value.includes(tagValue as never)) {
+          form.setValue("tags", [...field.value, tagValue]);
+          tagInput.value = "";
+          form.clearErrors("tags");
+        } else {
+          form.trigger();
+        }
+      }
+    }
+  };
+  const handleTagRemove = (tag: string, field: any) => {
+    const newTags = field.value.filter((t: string) => t !== tag);
+    form.setValue("tags", newTags);
+  };
 
   return (
     <Form {...form} className="flex w-full flex-col gap-10">
@@ -73,7 +107,6 @@ export default function Questions() {
                   apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
                   // @ts-ignore
                   onInit={(evt, editor) => (editorRef.current = editor)}
-                  initialValue="<p>This is the initial content of the editor.</p>"
                   init={{
                     height: 350,
                     menubar: false,
@@ -100,8 +133,8 @@ export default function Questions() {
                       "alignright alignjustify | bullist numlist ",
                     // eslint-disable-next-line camelcase
                     content_style: "body { font-family:Inter; font-size:16px }",
-                    // skin: mode === "dark" ? "oxide-dark" : "oxide",
-                    // content_css: mode === "dark" ? "dark" : "light",
+                    skin: mode === "dark" ? "oxide-dark" : "oxide",
+                    content_css: mode === "dark" ? "dark" : "light",
                   }}
                 />
               </FormControl>
@@ -115,17 +148,40 @@ export default function Questions() {
         />
         <FormField
           control={form.control}
-          name="tag"
+          name="tags"
           render={({ field }) => (
             <FormItem className="flex w-full flex-col">
               <FormLabel className="paragraph-semibold text-dark400_light800">
-                Tag <span className="text-primary-500">*</span>
+                Tags <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl className="mt-3.5">
-                <Input
-                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark400_light700 min-h-[56px] border"
-                  placeholder="Add tags..."
-                />
+                <>
+                  <Input
+                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark400_light700 min-h-[56px] border"
+                    placeholder="Add tags..."
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                  />
+                  {field.value.length > 0 && (
+                    <div className="flex-start mt-2.5 gap-2.5">
+                      {field.value.map((tag: any) => (
+                        <Badge
+                          key={tag}
+                          className="subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                          onClick={() => handleTagRemove(tag, field)}
+                        >
+                          <span>{tag} </span>
+                          <Image
+                            src="/assets/icons/close.svg"
+                            alt="close icon"
+                            width={12}
+                            height={12}
+                            className="cursor-pointer object-contain invert-0 dark:invert"
+                          />
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Add up to 3 tags to describe what your question is about. You
--- lib/validation.ts
+++ lib/validation.ts
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const questionSchema = z.object({
   title: z.string().min(2).max(150),
   explanation: z.string().min(150),
-  tag: z.array(z.string().min(1).max(25)).min(1).max(5),
+  tags: z.array(z.string().min(1).max(25)).min(1).max(5),
 });
