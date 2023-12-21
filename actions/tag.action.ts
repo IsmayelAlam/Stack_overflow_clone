05856137ce233d8dee703bc539b65c267dfd321a
Ismayel Alam <ismayelalam0@gmail.com>
2023-12-21 18:43:24 +0600

    add tag page

--- actions/tag.action.ts
+++ actions/tag.action.ts
@@ -1,6 +1,6 @@
 import { connectToDatabase } from "@/lib/mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
-// import Tag from "@/database/tag.model";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag from "@/database/tag.model";
 import User from "@/database/user.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
@@ -14,9 +14,21 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
     if (!user) throw new Error("User not found");
 
     return [
-      { _id: "1", name: "tag1" },
-      { _id: "2", name: "tag2" },
+      { _id: "1", name: "tag1", questions: ["test question"] },
+      { _id: "2", name: "tag2", questions: ["test question"] },
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tag = await Tag.find();
+
+    return tag;
   } catch (error) {
     console.log(error);
     throw error;
